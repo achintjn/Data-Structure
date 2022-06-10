450646733afe77de7d7851b1072aa1d50cd3d16d
aj7 <91110844+achintjn@users.noreply.github.com>
2022-06-10 15:07:38 -0500

    #189

---
+++ src/TwoSumII167.java
@@ -0,0 +1,17 @@
+public class TwoSumII167 {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int start = 0;
+        int end = numbers.length-1;
+
+        while(start<end){
+            if(numbers[start]+numbers[end]==target)
+                return new int[]{start+1, end+1};
+            if(numbers[start]+numbers[end]>target)
+                end--;
+            if(numbers[start]+numbers[end]<target)
+                start++;
+        }
+        return null;
+    }
+}
