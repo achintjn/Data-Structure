50356f6018c78a7fb096c392546ee4465505b8ee
aj7 <91110844+achintjn@users.noreply.github.com>
2022-06-11 02:04:02 -0500

    #19 second almost similar solution

--- src/RemoveNthNodefromLL19.java
+++ src/RemoveNthNodefromLL19.java
@@ -64,6 +64,31 @@ public class RemoveNthNodefromLL19 {
         return head;
     }
 
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(n>0){
+            fast = fast.next;
+            n--;
+        }
+        if(fast==null){
+            head = head.next;
+            return head;
+        }
+
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+
+        slow.next = slow.next.next;
+
+
+        return head;
+    }
+
     public static void main(String[] args) {
         ListNode node2 = new ListNode(2,null);
         ListNode node = new ListNode(1,node2);
