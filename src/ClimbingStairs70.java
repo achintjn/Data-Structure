1db3d74df8600b90663da03fda37dbc424515b2c
aj7 <91110844+achintjn@users.noreply.github.com>
2022-06-25 11:50:54 -0500

    #70 DP climbing stairs.

--- src/ClimbingStairs70.java
+++ src/ClimbingStairs70.java
@@ -36,7 +36,24 @@ public class ClimbingStairs70 {
 
     }
 
+    public static int climbStairs2(int n) {
+        int[] arr = new int[n];
+        return getCount(n, arr);
+    }
+
+    public static int getCount(int n, int[] arr){
+        if(n-1==0)
+            return 1;
+        if(n-2==0)
+            return 2;
+        if(arr[n-1]>0)
+            return arr[n-1];
+        arr[n-1] = getCount(n-1, arr) + getCount(n-2, arr);
+
+        return arr[n-1];
+    }
+
     public static void main(String[] args) {
-        System.out.println(climbStairs(2));
+        System.out.println(climbStairs2(3));
     }
 }
