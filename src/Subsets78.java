8db69a9353dc263af51f1a4835b8c0f411a6cd7c
aj7 <91110844+achintjn@users.noreply.github.com>
2022-07-12 23:00:38 -0500

    #78 iterative approach on subset

--- src/Subsets78.java
+++ src/Subsets78.java
@@ -22,6 +22,25 @@ public class Subsets78 {
         }
     }
 
+    //iterative approach my logic
+    public List<List<Integer>> subsets2(int[] nums) {
+
+        LinkedList<List<Integer>> list= new LinkedList<>();
+        LinkedList<Integer> list2 =  new LinkedList<>();
+        list.add(list2);
+
+        for(int i:nums){
+            LinkedList<List<Integer>> local= new LinkedList<>();
+            for(List<Integer> a:list){
+                local.add(new LinkedList<Integer>(a){{add(i);}});
+            }
+            for(List<Integer> a:local){
+                list.add(a);
+            }
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1,2,3};
 
