42d32f63b52843f79501b9ec511454382dcf750f
aj7 <91110844+achintjn@users.noreply.github.com>
2022-07-13 23:36:37 -0500

    #90 second solution is incomplete

--- src/SubsetsII90.java
+++ src/SubsetsII90.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class SubsetsII90 {
 
     //backtracking
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
 
         LinkedList<List<Integer>> list= new LinkedList<>();
         LinkedList<Integer> list2 =  new LinkedList<>();
@@ -14,15 +14,48 @@ public class SubsetsII90 {
         return list;
     }
 
-    public void subsets(int n, int[] nums,LinkedList<List<Integer>> list,LinkedList<Integer> list2){
+    public static void subsets(int n, int[] nums,LinkedList<List<Integer>> list,LinkedList<Integer> list2){
 
-        if(!(list.contains(list2)))
+        //if(!(list.contains(list2)))
             list.add(new LinkedList(list2));
 
         for(int i=n;i<nums.length;i++){
+
+            if (i != n && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
             list2.add(nums[i]);
             subsets(i+1, nums, list, list2);
             list2.removeLast();
         }
+    }
+    //need to work on this
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        Arrays.sort(nums);
+        LinkedList<List<Integer>> list= new LinkedList<>();
+        LinkedList<Integer> list2 =  new LinkedList<>();
+        list.add(list2);
+
+        int x = 0;
+
+        for(int i:nums){
+            if(i==x)
+                continue;
+            else
+                x=i;
+            LinkedList<List<Integer>> local= new LinkedList<>();
+            for(List<Integer> a:list){
+                local.add(new LinkedList<Integer>(a){{add(i);}});
+            }
+            for(List<Integer> a:local){
+                list.add(a);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subsetsWithDup(new int[]{1,2,2}));
     }
 }
