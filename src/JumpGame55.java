0c852f9beae97ef91e33d37ba5d3e651e1cb443f
aj7 <91110844+achintjn@users.noreply.github.com>
2022-07-26 01:44:01 -0400

    #55

--- src/JumpGame55.java
+++ src/JumpGame55.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 public class JumpGame55 {
+    //top down recursive approach
+    int min  = Integer.MAX_VALUE;
     public static boolean canJump(int[] nums) {
         int[] arr = new int[nums.length];
         Arrays.fill(arr,-1);
@@ -31,7 +33,49 @@ public class JumpGame55 {
         return arr[i];
     }
 
+    enum Index {
+        GOOD, BAD, UNKNOWN
+    }
+        //leet code solution
+        public static boolean canJump2(int[] nums) {
+            Index[] memo = new Index[nums.length];
+            for (int i = 0; i < memo.length; i++) {
+                memo[i] = Index.UNKNOWN;
+            }
+            memo[memo.length - 1] = Index.GOOD;
+
+            for (int i = nums.length - 2; i >= 0; i--) {
+                int furthestJump = Math.min(i + nums[i], nums.length - 1);
+                for (int j = i + 1; j <= furthestJump; j++) {
+                    if (memo[j] == Index.GOOD) {
+                        memo[i] = Index.GOOD;
+                        break;
+                    }
+                }
+            }
+
+            return memo[0] == Index.GOOD;
+        }
+
+        //bottom up iterative approach
+    public static boolean canJump3(int[] nums) {
+        int[] arr = new int[nums.length];
+        Arrays.fill(arr,-1);
+        arr[nums.length-1] = 1;
+
+        for(int i=nums.length-2;i>=0;i--){
+            for(int j=i;j<i+nums[i];j++){
+                if(arr[j+1]==1)
+                {
+                    arr[i]=1;
+                    break;
+                }
+            }
+        }
+        return (arr[0]==1)?true:false;
+    }
+
     public static void main(String[] args) {
-        System.out.println(canJump(new int[]{2,3,1,1,4}));
+        System.out.println(canJump3(new int[]{2,0,0}));
     }
 }
