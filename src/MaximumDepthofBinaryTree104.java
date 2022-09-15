654f0f11cd0dbaa7eae73cfa9e9a3e44e643c5d4
aj7 <91110844+achintjn@users.noreply.github.com>
2022-09-15 12:39:42 -0400

    #104 tree easy BFS

--- src/MaximumDepthofBinaryTree104.java
+++ src/MaximumDepthofBinaryTree104.java
@@ -1,4 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDepthofBinaryTree104 {
+    //DFS
     public int maxDepth(TreeNode root) {
         int depth=0;
         return maxDepth(root,depth);
@@ -11,6 +15,27 @@ public class MaximumDepthofBinaryTree104 {
         depth++;
         depth = Math.max(maxDepth(root.left, depth),maxDepth(root.right, depth));
 
+        return depth;
+    }
+
+    //BFS
+    public int maxDepth2(TreeNode root) {
+        if(root==null)
+            return 0;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        int depth=0;
+        while(!que.isEmpty()){
+            int size =  que.size();
+            depth++;
+            for(int i=0;i<size;i++){
+                TreeNode curr = que.poll();
+                if(curr.right!=null)
+                    que.add(curr.right);
+                if(curr.left!=null)
+                    que.add(curr.left);
+            }
+        }
         return depth;
     }
 }
