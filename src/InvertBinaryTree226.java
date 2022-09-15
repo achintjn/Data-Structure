e8c023516dfaff64a01b8d0063687b6424ee1c31
aj7 <91110844+achintjn@users.noreply.github.com>
2022-09-15 11:20:04 -0400

    #226 tree easy

---
+++ src/InvertBinaryTree226.java
@@ -0,0 +1,18 @@
+public class InvertBinaryTree226 {
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root==null)
+            return null;
+
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode temp = root.left;
+        root.left=root.right;
+        root.right = temp;
+
+        return root;
+
+    }
+}
