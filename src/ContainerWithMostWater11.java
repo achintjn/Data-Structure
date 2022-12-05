ed0550bc6e00f02fd0b677ed338a2070276a9f7e
aj7 <91110844+achintjn@users.noreply.github.com>
2022-12-05 16:02:21 -0500

    #

--- src/ContainerWithMostWater11.java
+++ src/ContainerWithMostWater11.java
@@ -16,4 +16,24 @@ public class ContainerWithMostWater11 {
         }
         return max;
     }
+    //revise
+    public static int maxArea2(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int area = 0;
+
+        while(left<right){
+            int curr_area = (right-left)*(Math.min(height[left],height[right]));
+            area = Math.max(area, curr_area);
+            if(height[right]>height[left])
+                left++;
+            else
+                right--;
+        }
+        return area;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea2(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
 }
