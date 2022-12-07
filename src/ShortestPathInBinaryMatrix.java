d0d91acacd5d60dbe1976821b5e0e1f2ed8149de
aj7 <91110844+achintjn@users.noreply.github.com>
2022-12-06 19:00:51 -0500

    #

--- src/ShortestPathInBinaryMatrix.java
+++ src/ShortestPathInBinaryMatrix.java
@@ -10,7 +10,8 @@ public class ShortestPathInBinaryMatrix {
 
         int [] dir = new int[]{-1,-1,1,1,-1,0,1,0,-1};
         Queue<int[]> que =  new LinkedList<>();
-        que.add(new int[]{0,0});grid[0][0]=1;
+        que.add(new int[]{0,0});
+        grid[0][0]=1;
         while(!(que.isEmpty())){
             int [] rc = que.poll();
             int r = rc[0];
@@ -35,7 +36,46 @@ public class ShortestPathInBinaryMatrix {
 
     }
 
+    public static int shortestPathBinaryMatrix2(int[][] grid) {
+        if(grid[0][0]!=0)
+            return -1;
+        int[][] distance = new int[grid.length][grid[0].length];
+
+                bfs(0,0, grid,distance);
+
+
+        if(distance[grid.length-1][grid[0].length-1]==0)
+            return -1;
+        else
+            return distance[grid.length-1][grid[0].length-1];
+
+    }
+
+    public static void bfs(int r, int c, int[][] grid,int[][] distance){
+        distance[0][0] =1;
+        int[][] dir = new int[][]{{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}};
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{r,c});
+        while(!que.isEmpty()){
+            int[] arr = que.poll();
+            int r1 = arr[0];
+            int c1 = arr[1];
+            for(int i=0;i<dir.length;i++){
+                int a = r1+dir[i][0];
+                int b = c1+dir[i][1];
+                if(a<0 || b<0 || a>grid.length-1 || b>grid.length-1 || grid[a][b]!=0)
+                    continue;
+                else{
+                    if(distance[a][b]==0 || distance[a][b]>1+distance[r1][c1]) {
+                        distance[a][b]=1+distance[r1][c1];
+                        que.add(new int[]{a,b});
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(shortestPathBinaryMatrix(new int[][]{{0,0,0},{1,1,0},{1,1,0}}));
+        System.out.println(shortestPathBinaryMatrix2(new int[][]{{0,1},{1,0}}));
     }
 }
