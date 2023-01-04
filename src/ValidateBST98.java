52dc53244a83cee3dc83972d5773f3c14f1289b1
aj7 <91110844+achintjn@users.noreply.github.com>
2023-01-04 13:45:30 -0500

    #

--- src/ValidateBST98.java
+++ src/ValidateBST98.java
@@ -10,4 +10,26 @@ public class ValidateBST98 {
             return false;
         return isValidBST(root.left,gt,root.val) && isValidBST(root.right,root.val,lt);
     }
+
+    //using inorder traversal
+    //every element should be less then next one
+    Integer val = null;
+    public boolean isValidBST2(TreeNode root) {
+
+        return isValid(root);
+    }
+
+    public boolean isValid(TreeNode root){
+        if(root==null)
+            return true;
+        if(!isValid(root.left))
+            return false;
+        if(val!=null && val>=root.val)
+            return false;
+        val = root.val;
+        if(!isValid(root.right))
+            return false;
+
+        return true;
+    }
 }
