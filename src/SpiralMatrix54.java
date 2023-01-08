fea8e2b13c60aa6fc98daf1a3ef630767000484e
aj7 <91110844+achintjn@users.noreply.github.com>
2023-01-08 12:24:13 -0500

    #54 new soln

--- src/SpiralMatrix54.java
+++ src/SpiralMatrix54.java
@@ -28,6 +28,46 @@ public class SpiralMatrix54 {
         dfs(r+dir[n],c+dir[n+1],matrix,list,dir,n);
     }
 
+    //better soln
+    public List<Integer> spiralOrder2(int[][] matrix) {
+        List<Integer> list = new LinkedList<>();
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int right = cols-1;
+        int left = 0;
+        int down = rows-1;
+        int up = 0;
+
+        while(list.size()<rows*cols){
+            for(int i=left;i<=right;i++){
+                list.add(matrix[up][i]);
+            }
+
+            for(int i=up+1;i<=down;i++){
+                list.add(matrix[i][right]);
+            }
+            if(up!=down){
+                for(int i=right-1;i>=left;i--){
+                    list.add(matrix[down][i]);
+                }
+
+            }
+            if(left!=right){
+                for(int i=down-1;i>up;i--){
+                    list.add(matrix[i][left]);
+                }
+            }
+
+            up++;
+            left++;
+            right--;
+            down--;
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         System.out.println(spiralOrder(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
     }
