8546c677911a5dfd2a7aa3921362d68fa11355f7
aj7 <91110844+achintjn@users.noreply.github.com>
2023-01-19 18:40:47 -0500

    #73

--- src/SetMatrixZeroes73.java
+++ src/SetMatrixZeroes73.java
@@ -3,19 +3,40 @@ import java.util.Arrays;
 public class SetMatrixZeroes73 {
     public static void setZeroes(int[][] matrix) {
 
-        for(int r=0; r<matrix.length;r++){
-            for(int c=0;c<matrix[r].length;c++){
-                if(matrix[r][c]==0){
-                    matrix[0][c]=0;
-                    matrix[r][0]=0;
+        Boolean isCol = false;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int i = 0; i < R; i++) {
+
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < C; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        for(int r=0;r<matrix.length;r++){
-            for(int c=0;c<matrix[r].length;c++){
-                if(matrix[r][0]==0 || matrix[0][c]==0)
-                    matrix[r][c]=0;
+        for(int i=1;i<R;i++){
+            for(int j=1;j<C;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int i=0;i<C;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(isCol){
+            for(int i=0;i<R;i++){
+                matrix[i][0]=0;
             }
         }
 
