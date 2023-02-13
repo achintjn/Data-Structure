f6bebbddcf60cf028d68f73384fa7e586d242d38
achintjn <achintjain66@gmail.com>
2023-02-13 10:34:00 -0500

    #159 Longest substring with 2 distinct char

---
+++ src/LongestSubStringWIthAtMost2DistinctChar159.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStringWIthAtMost2DistinctChar159 {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int left = 0;
+        //int right =2;
+        int max = 1;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0),0);
+        char last = s.charAt(0);
+        for(int i=1;i<s.length();i++){
+            map.put(s.charAt(i),i);
+            if(map.size()==3){
+                // delete the leftmost character
+                int del_idx = Collections.min(map.values());
+                map.remove(s.charAt(del_idx));
+                // move left pointer of the slidewindow
+                left = del_idx + 1;
+            }
+            max = Math.max(max, i-left+1);
+        }
+        return max;
+    }
+}
