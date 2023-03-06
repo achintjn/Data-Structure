3a222d2a17612660cf98da6861254574fb3e8536
achintjn <achintjain66@gmail.com>
2023-03-06 18:25:55 -0500

    #

--- src/InsertInterval57.java
+++ src/InsertInterval57.java
@@ -3,19 +3,20 @@ import java.util.LinkedList;
 public class InsertInterval57 {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         LinkedList<int[]> list = new LinkedList<>();
+        int ADDED = 1000000;
         for(int[] curr:intervals){
             if(newInterval[1]<curr[0]){
                 list.add(new int[]{newInterval[0],newInterval[1]});
                 list.add(curr);
-                newInterval[0]=newInterval[1]=1000000;
-            }else if(newInterval[0]==1000000 || newInterval[0]>curr[1]){
+                newInterval[0]=newInterval[1]=ADDED;
+            }else if(newInterval[0]==ADDED || newInterval[0]>curr[1]){
                 list.add(curr);
             }else{
                 newInterval[0]=Math.min(newInterval[0],curr[0]);
                 newInterval[1]=Math.max(newInterval[1],curr[1]);
             }
         }
-        if(newInterval[0]!=1000000)
+        if(newInterval[0]!=ADDED)
             list.add(newInterval);
         return list.toArray(new int[list.size()][]);
     }
