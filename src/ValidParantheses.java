8cf8761d538cfad7db3dbb5c71bd14da2708bbd1
achintjn <achintjain66@gmail.com>
2023-06-03 21:13:12 -0500

    #second solution

--- src/ValidParantheses.java
+++ src/ValidParantheses.java
@@ -27,4 +27,25 @@ public class ValidParantheses {
         return st.isEmpty()?true:false;
 
     }
+
+    public boolean isValid2(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c:s.toCharArray()){
+            if(!stack.isEmpty() && c==map.getOrDefault(stack.peek(),'.'))
+                stack.pop();
+            else
+                stack.push(c);
+        }
+
+        if(stack.isEmpty())
+            return true;
+        return false;
+    }
 }
