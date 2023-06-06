cb7bb0f8ec07eda9d03e75a46c0dd8f7f9998af4
achintjn <achintjain66@gmail.com>
2023-06-06 14:08:55 -0500

    #concise soln

--- src/KokoEatingBananas875.java
+++ src/KokoEatingBananas875.java
@@ -24,6 +24,32 @@ public class KokoEatingBananas875 {
         return a;
     }
 
+
+    //concise and accurate solution
+    public int minEatingSpeed2023(int[] piles, int h) {
+
+        int max = 0;
+        for(int a:piles)
+            max = Math.max(max,a);
+
+        int start = 1;
+        int end = max;
+
+        while(start<end){
+            int k = start + (end-start)/2;
+            int h0=0;
+            for(int pile:piles){
+                h0 = h0 + (int)Math.ceil((double)pile/k);
+            }
+            if(h0>h)
+                start = k+1;
+            else if(h0<=h)
+                end = k;
+        }
+
+        return end;
+    }
+
     public static void main(String[] args) {
         System.out.println(minEatingSpeed(new int[]{1000000000},2));
     }
