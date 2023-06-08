4c60d356f792eb5ef9f1129b2b6acb6596f1a896
achintjn <achintjain66@gmail.com>
2023-06-08 17:12:48 -0500

    #new submission

--- src/AddTwoNumbers2.java
+++ src/AddTwoNumbers2.java
@@ -36,5 +36,33 @@ public class AddTwoNumbers2 {
             return head.next;
 
         }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+
+        ListNode l3 = new ListNode(0);
+        ListNode res = l3;
+
+        int carry = 0;
+        while(l2!=null || l1!=null){
+            int x = l1==null?0:l1.val;
+            int y = l2==null?0:l2.val;
+            int cal = x + y + carry;
+            carry=0;
+            if(cal>=10){
+                cal = cal-10;
+                carry = 1;
+            }
+            l3.next = new ListNode(cal);
+            l3 = l3.next;
+            l2 = l2==null?null:l2.next;
+            l1 = l1==null?null:l1.next;
+        }
+
+        if(carry!=0)
+            l3.next = new ListNode(carry);
+
+        return res.next;
+
+    }
     }
 
