84011143cbfc320e55263d581f8349b86ca78c7a
achintjn <achintjain66@gmail.com>
2023-06-15 14:45:46 -0500

    #Balanced Binary tree soln2

--- src/BalancedBinaryTree110.java
+++ src/BalancedBinaryTree110.java
@@ -1,24 +1,40 @@
 public class BalancedBinaryTree110 {
     boolean isdiff = true;
+
     public boolean isBalanced(TreeNode root) {
         height(root);
         return isdiff;
     }
 
-    public int height(TreeNode root){
-        if(root==null)
+    public int height(TreeNode root) {
+        if (root == null)
             return 0;
-        if(!isdiff)
+        if (!isdiff)
             return 0;
-        int left = height(root.left)+1;
-        if(!isdiff)
+        int left = height(root.left) + 1;
+        if (!isdiff)
             return 0;
-        int right = height(root.right)+1;
+        int right = height(root.right) + 1;
 
-        if(Math.abs(left-right)>1)
+        if (Math.abs(left - right) > 1)
             isdiff = false;
-        return Math.max(left,right);
+        return Math.max(left, right);
     }
 
+    public boolean isBalanced2(TreeNode root) {
 
+        return isHeight(root) != -1;
+    }
+
+    public int isHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = isHeight(root.left);
+        int right = isHeight(root.right);
+
+        if (Math.abs(left - right) > 1 || left == -1 || right == -1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
 }
