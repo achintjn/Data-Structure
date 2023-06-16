c8222c2eb1388fab98e9bd85236f7e21467f09f6
achintjn <achintjain66@gmail.com>
2023-06-16 16:33:00 -0500

    #little tweak

--- src/CopyListWithRandomPointer138.java
+++ src/CopyListWithRandomPointer138.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class CopyListWithRandomPointer138 {
 
 // Definition for a Node.
-class Node {
+static class Node {
     int val;
     Node next;
     Node random;
@@ -16,9 +16,7 @@ class Node {
     }
 }
 
-
-    class Solution {
-        public Node copyRandomList(Node head) {
+        public static Node copyRandomList(Node head) {
             if(head==null)
                 return head;
             Map<Node, Node> map = new HashMap<>();
@@ -44,5 +42,23 @@ class Node {
             }
             return copyHead;
         }
+
+    public static void main(String[] args) {
+        Node node = new Node(7);
+        Node node1 = new Node(13);
+        Node node2 = new Node(11);
+        Node node3 = new Node(10);
+        Node node4 = new Node(1);
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node1.random = node;
+        node2.random = node4;
+        node3.random = node2;
+        node4.random = node1;
+
+        copyRandomList(node);
+
     }
 }
