9874758e2fc626ce9bbd62641368753bb1a10666
achintjn <achintjain66@gmail.com>
2023-07-12 23:48:17 -0500

    #261 Graph valid tree

--- src/GraphValidTree.java
+++ src/GraphValidTree.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GraphValidTree {
@@ -36,5 +37,40 @@ public class GraphValidTree {
             }
         }
         return false;
+    }
+
+
+
+    private List<List<Integer>> adjacencyList = new ArrayList<>();
+    private Set<Integer> seen2 = new HashSet<>();
+
+
+    public boolean validTree2(int n, int[][] edges) {
+
+        if (edges.length != n - 1) return false;
+
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+
+        // We return true iff no cycles were detected,
+        // AND the entire graph has been reached.
+        return dfs2(0, -1) && seen2.size() == n;
+    }
+
+    public boolean dfs2(int node, int parent) {
+        if (seen2.contains(node)) return false;
+        seen2.add(node);
+        for (int neighbour : adjacencyList.get(node)) {
+            if (parent != neighbour) {
+                boolean result = dfs2(neighbour, node);
+                if (!result) return false;
+            }
+        }
+        return true;
     }
 }
